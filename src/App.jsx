b3f42936c4dc7b466004df98ec40e58c22a2767d
unknown <skyanant123@gmail.com>
2025-07-06 22:22:38 +0530

    fix: correct Navbar filename for Vercel (case-sensitive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./components/NavbarTemp";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import ExploreSection from "./components/ExploreSection";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <div className="flex flex-col items-center justify-center min-h-[80vh] bg-gray-50">
         <h1 className="text-4xl font-bold text-blue-700 fade-in-slide-up">
           Welcome to SkillSwap 👋
